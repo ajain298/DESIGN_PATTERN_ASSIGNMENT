56be5db9430f1e32b6ae7623bbf49d56d6796787
Aakanksha Jain <ajain251@asu.edu>
2022-10-19 12:40:45 -0700

    update

--- src/Facade.java
+++ src/Facade.java
@@ -1,14 +1,47 @@
 import java.util.Scanner;
 public class Facade {
+    String UserName;
+    String Password;
     private int UserType;
     private Product theSelectedProduct;
     private int nProductCategory;
     private ProductList theProductList;
     private Person thePerson;
 
-    public int login(LoginForUser l)
+    public Facade() {
+    }
+
+    public int login()
     {
-        return l.login();
+        System.out.println("Enter your Username : ");
+        Scanner scan = new Scanner(System.in);
+        UserName = scan.next();
+        System.out.println("Enter your Password : ");
+        Password = scan.next();
+
+        if((UserName.equalsIgnoreCase("tutu")) && (Password.equalsIgnoreCase("1111")))
+        {
+            UserType = 0;
+        }
+        else if((UserName.equalsIgnoreCase("mimi")) && (Password.equalsIgnoreCase("2222")))
+        {
+            UserType = 0;
+        }
+        else if((UserName.equalsIgnoreCase("nana")) && (Password.equalsIgnoreCase("3333")))
+        {
+            UserType = 0;
+        }
+        else if((UserName.equalsIgnoreCase("pepe")) && (Password.equalsIgnoreCase("3333")))
+        {
+            UserType = 1;
+        }
+        else
+        {
+            System.out.println("No such user found !!!");
+            System.exit(-1);
+        }
+        return UserType;
+
     }
 
     public void createUser(UserInfoItem userinfoitem)
@@ -52,6 +85,11 @@ public class Facade {
     }
 
     public void remind(){
+
+    }
+
+    public void begin()
+    {
 
     }
 
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
-public class Main {
+public class Main
+{
     public static void main(String[] args)
     {
-
-
-
+        Facade f = new Facade();
+        f.begin();
     }
 }
