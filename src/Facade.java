85b81ae621acc3599d43a6cc0acce8db8278256d
Aakanksha Jain <ajain251@asu.edu>
2022-10-19 17:21:08 -0700

    update Iterator

--- src/Facade.java
+++ src/Facade.java
@@ -132,6 +132,7 @@ public class Facade {
         Iterator I2 = OL.getIterator();
         for(I1 = PL.getIterator();p.hasNext(I1);)
         {
+            //String s = (String) p.Nextp(I1);
             System.out.println(p.Nextp(I1));
             System.out.println(o.Nexto(I2));
 
